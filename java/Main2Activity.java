580eb48c9e2ec5194c7f74ef49742d16d3170ab6
sudhiawasthi <35043619+sudhiawasthi@users.noreply.github.com>
2019-01-24 20:17:34 +0530

    Add files via upload

---
+++ java/Main2Activity.java
@@ -0,0 +1,86 @@
+package com.example.android.miwokk;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+    MediaPlayer mplayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+        final ArrayList<Word> words=new ArrayList<Word>();
+
+        words.add(new Word("one","lutti",R.drawable.number_one,R.raw.number_one));
+        words.add(new Word("two","otiiko",R.drawable.number_two,R.raw.number_two));
+        words.add(new Word("three","tolookosu",R.drawable.number_three,R.raw.number_three));
+        words.add(new Word("four","oyyisa",R.drawable.number_four,R.raw.number_four));
+        words.add(new Word("five","massokka",R.drawable.number_five,R.raw.number_five));
+        words.add(new Word("six","temmokka",R.drawable.number_six,R.raw.number_six));
+        words.add(new Word("seven","kenekaku",R.drawable.number_seven,R.raw.number_seven));
+        words.add(new Word("eight","kawinta",R.drawable.number_eight,R.raw.number_eight));
+        words.add(new Word("nine","wo'e",R.drawable.number_nine,R.raw.number_nine));
+        words.add(new Word("ten","naa'aacha",R.drawable.number_ten,R.raw.number_ten));
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+             mplayer =MediaPlayer.create(Main2Activity.this,words.get(position).getmAudioid());
+                    mplayer.start();
+                    mplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            releaseMediaPlayer();
+                        }
+                    });
+                }
+
+
+        });
+
+
+    }
+
+    private void releaseMediaPlayer(){
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mplayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mplayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mplayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+}
