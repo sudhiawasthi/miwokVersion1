23447c36546c54a2c8f5236a2e0780112bc27e29
sudhiawasthi <35043619+sudhiawasthi@users.noreply.github.com>
2019-01-29 13:31:08 +0530

    Update Main2Activity.java
    Updated audio focus change listener

--- java/Main2Activity.java
+++ java/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.android.miwokk;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,50 +20,34 @@ import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
     MediaPlayer mplayer;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    AudioManager mAudioManager;
+    private AudioManager.OnAudioFocusChangeListener mFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT|| focusChange== AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+            {
+                // we are dealing both cases together because our audio file is small and hence we will pause and play from beginning in bOTH CASES
 
-        setContentView(R.layout.activity_main2);
-        final ArrayList<Word> words=new ArrayList<Word>();
-
-        words.add(new Word("one","lutti",R.drawable.number_one,R.raw.number_one));
-        words.add(new Word("two","otiiko",R.drawable.number_two,R.raw.number_two));
-        words.add(new Word("three","tolookosu",R.drawable.number_three,R.raw.number_three));
-        words.add(new Word("four","oyyisa",R.drawable.number_four,R.raw.number_four));
-        words.add(new Word("five","massokka",R.drawable.number_five,R.raw.number_five));
-        words.add(new Word("six","temmokka",R.drawable.number_six,R.raw.number_six));
-        words.add(new Word("seven","kenekaku",R.drawable.number_seven,R.raw.number_seven));
-        words.add(new Word("eight","kawinta",R.drawable.number_eight,R.raw.number_eight));
-        words.add(new Word("nine","wo'e",R.drawable.number_nine,R.raw.number_nine));
-        words.add(new Word("ten","naa'aacha",R.drawable.number_ten,R.raw.number_ten));
-        WordAdapter adapter = new WordAdapter(this, words);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(adapter);
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mplayer.pause();
+            mplayer.seekTo(0); // the audio is set at the 0 msec so that whenever it is played again, it plays from the start
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_LOSS)
+            {
+                //agar milna hi nhi ho to  rsource clean krdo
                 releaseMediaPlayer();
-             mplayer =MediaPlayer.create(Main2Activity.this,words.get(position).getmAudioid());
-                    mplayer.start();
-                    mplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            releaseMediaPlayer();
-                        }
-                    });
-                }
-
-
-        });
-
-
-    }
-
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_GAIN)
+            {
+                mplayer.start();
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener mCompletionListener =new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
     private void releaseMediaPlayer(){
         // If the media player is not null, then it may be currently playing a sound.
         if (mplayer != null) {
@@ -76,11 +61,52 @@ public class Main2Activity extends AppCompatActivity {
             mplayer = null;
         }
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "naa'aacha", R.drawable.number_ten, R.raw.number_ten));
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+                int result = mAudioManager.requestAudioFocus(mFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mplayer = MediaPlayer.create(Main2Activity.this, words.get(position).getmAudioid());
+                    mplayer.start();
+                    mplayer.setOnCompletionListener(mCompletionListener);
+                }
+
+
+            }
+
+
+        });
+    }
 
     @Override
     protected void onStop() {
         super.onStop();
         releaseMediaPlayer();
     }
-
-}
+    }
