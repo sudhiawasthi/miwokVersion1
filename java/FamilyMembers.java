28af09af2ecb880a7493ddddd69639bdeddb762d
sudhiawasthi <35043619+sudhiawasthi@users.noreply.github.com>
2019-01-29 13:29:06 +0530

    Update FamilyMembers.java
    Updated audio focus

--- java/FamilyMembers.java
+++ java/FamilyMembers.java
@@ -1,5 +1,6 @@
 package com.example.android.miwokk;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,35 @@ import java.util.ArrayList;
 
 public class FamilyMembers extends AppCompatActivity {
     MediaPlayer mplayer;
+    AudioManager mAudioManager;
+    private AudioManager.OnAudioFocusChangeListener mFocusChangeListener= new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT|| focusChange== AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+            {
+                // we are dealing both cases together because our audio file is small and hence we will pause and play from beginning in bOTH CASES
+
+                mplayer.pause();
+                mplayer.seekTo(0); // the audio is set at the 0 msec so that whenever it is played again, it plays from the start
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_LOSS)
+            {
+                //agar milna hi nhi ho to  rsource clean krdo
+                releaseMediaPlayer();
+            }
+            else if(focusChange==AudioManager.AUDIOFOCUS_GAIN)
+            {
+                mplayer.start();
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,17 +70,34 @@ public class FamilyMembers extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
                 releaseMediaPlayer();
-                mplayer =MediaPlayer.create(FamilyMembers.this,words.get(position).getmAudioid());
-                mplayer.start();
-                mplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        releaseMediaPlayer();
-                    }
-                });
 
+                // Get the {@link Word} object at the given position the user clicked on
+                Word word = words.get(position);
+
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have audio focus now.
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mplayer = MediaPlayer.create(FamilyMembers.this, word.getmAudioid());
+
+                    // Start the audio file
+                    mplayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mplayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
 
